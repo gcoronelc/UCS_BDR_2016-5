c7e299489529cca91232c6272168d3e841fd1e8c
Docente <Docente>
2016-09-22 22:39:03 -0500

    Lec 12 y 13

---
+++ Lec13/Respuestas.sql
@@ -0,0 +1,9 @@
+-- SELECCION DE COLUMNAS
+-- ==========================
+
+-- Desarrolle una sentencia SELECT para consultar el 
+-- id, nombre, apellido y email de todos los alumnos. 
+-- Base de datos EDUTEC. 
+
+SELECT IdAlumno, NomAlumno, ApeAlumno, EmailAlumno
+FROM EduTec.DBO.Alumno;
